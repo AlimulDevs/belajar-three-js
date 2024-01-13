93fe21d702ac8f5ecfadc64bf5834824d8cebb03
alimul <alimuldev@gmail.com>
2024-01-13 19:32:14 +0700

    add

--- tutorial-2/index.js
+++ tutorial-2/index.js
@@ -1,4 +1,4 @@
-import * as THREE from "../node_modules/three/build/three.module.js"
+import * as THREE from "three/build/three.module.js"
 
 var scene = new THREE.Scene();
 var cam = new THREE.PerspectiveCamera(45, innerWidth / innerHeight, 1, 100);
